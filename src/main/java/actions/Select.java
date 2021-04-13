d711238f7d700d81d3359611ff36a0e852982fc7
Sundaresan Jeyamani <sundaresan.jeyamani@unionbankph.com>
2021-04-13 15:23:57 +0530

    select class

--- src/main/java/actions/Select.java
+++ src/main/java/actions/Select.java
@@ -1,25 +1,68 @@
 package actions;
 
+import base.Keywords;
 import exceptions.ApplicationException;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import base.Keywords;
-import org.openqa.selenium.By;
 
-public class Return extends Keywords {
+public class Select extends Keywords {
 
-    private static Logger log=Logger.getLogger(Return.class);
+    private static Logger log=Logger.getLogger(Select.class);
 
-    public static void clickEnter(String locatorkey) throws ApplicationException
+    public static void backOffice_selectValueInAnyList(String locatorkey) throws ApplicationException
     {
         try{
-            get.elementBy(locatorkey).sendKeys(Keys.ENTER);
+
+                    String Datatoselect = locatorkey.trim();
+                    Thread.sleep(1000);
+                    TestAttributes.element.click();
+                    Thread.sleep(1000);
+                    //String Datatoselectxpath= TestAttributes.LocatorValue+"//*[text()="+"'"+Datatoselect+"'"+"]";
+                    //TestAttributes.element.sendKeys(Keys.DOWN);
+                    String Datatoselectxpath= "("+"//div[text()="+"'"+Datatoselect+"'"+"]"+")"+"[1]" +"|"+ "("+"//li[text()="+"'"+Datatoselect+"'"+"]"+")"+"[1]" +"|"+ "("+"//a[text()="+"'"+Datatoselect+"'"+"]"+")"+"[1]";
+                    TestAttributes.driver.findElement(By.xpath(Datatoselectxpath)).click();
+                    Thread.sleep(100);
+                    TestAttributes.ActualResult = "Successfully Value selected from Listbox";
+                }
+                catch(Exception e){
+
+                    try {
+
+                        //TestAttributes.element.click();
+                        Thread.sleep(100);
+                        //TestAttributes.element.click();
+                        String Datatoselect = TestAttributes.Data.trim();
+                        String Datatoselectxpath= "("+"//*[text()="+"'"+Datatoselect+"'"+"]"+")"+"[1]";
+                        //Actions action= new Actions(TestAttributes.driver);
+                        //action.moveToElement(TestAttributes.element).build().perform();
+                        TestAttributes.driver.findElement(By.xpath(Datatoselectxpath)).click();
+                        //Thread.sleep(2000);
+                        TestAttributes.ActualResult = "Successfully Value selected from Listbox";
+                    } catch (Exception e1) {
+                        // TODO Auto-generated catch block
+                        try {
+//				          Thread.sleep(100);
+
+                            TestAttributes.element.click();
+                             Robot robot = new Robot();
+//			    robot.keyPress(KeyEvent.VK_DOWN);
+//			    robot.keyPress(KeyEvent.VK_DOWN);
+                            robot.keyPress(KeyEvent.VK_ENTER);
+
+                            TestAttributes.ActualResult = "Successfully Value selected from Listbox";
+                        }
+                        catch(InterruptedException e2){
+
+
+                            e2.printStackTrace();
+                        }
+
+
+                    }
+                }
+            }
         }
-        catch(NullPointerException e)
-        {
-            log.warn("Failed to execute Enter key");
-    }
+
 
     }
 }
