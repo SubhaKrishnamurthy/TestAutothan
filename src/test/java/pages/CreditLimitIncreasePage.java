776680d563c47e343552d6b913b0b8f5d6d9e9c1
Subha Krishnamoorthy <skrishnamoorthy@unionbankph.com>
2022-05-19 16:16:44 +0530

    Rajasekar commit_05/19/2022 - xpath updated

--- src/test/java/pages/CreditLimitIncreasePage.java
+++ src/test/java/pages/CreditLimitIncreasePage.java
@@ -1,11 +1,40 @@
 package pages;
 
+import actions.Wait;
 import base.Keywords;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class CreditLimitIncreasePage extends Keywords {
 
+    private String CreditcardAccount  = "onlineBanking.CreditLimit.CreditcardAccount";
+    private String RequestIncreaseButton  = "onlineBanking.CreditLimit.RequestIncreaseButton";
+    private String RIPopupMessage  = "onlineBanking.CreditLimit.RIPopupMessage";
+    private String keygotit="onlineBanking.Fundtransfer.BtnGotit";
 
-
-
+    public void click_CreditCardAccount() throws Throwable {
+        Wait.forSeconds(5000);
+        click.elementBy(CreditcardAccount);
+    }
+    public void click_RequestIncrease() throws Throwable {
+        Wait.forSeconds(3000);
+        jsClick.elementBy(RequestIncreaseButton);
+    }
+    public void Verify_RIPopupMessage() throws Throwable {
+        Wait.forSeconds(5000);
+        verify.elementIsPresent(RIPopupMessage);
+    }
+    public void click_GotIt() throws Throwable {
+        Wait.forSeconds(2000);
+        jsClick.elementBy(keygotit);
+    }
+    public void click_Slider() throws Throwable {
+        Wait.forSeconds(2000);
+        WebElement slider = driver.findElement(By.xpath("//div[@class='ant-slider-handle']"));
+        Actions move = new Actions(driver);
+        Actions action = (Actions) move.dragAndDropBy(slider, 30, 0).build();
+        action.perform();
+    }
 
 }
--- src/test/java/stepdefinitions/CreditLimitIncrease.java
+++ src/test/java/stepdefinitions/CreditLimitIncrease.java
@@ -3,27 +3,31 @@ package stepdefinitions;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import pages.CreditLimitIncreasePage;
 
 public class CreditLimitIncrease {
+
+    CreditLimitIncreasePage CreditLimitIncreasePage =new CreditLimitIncreasePage();
+
     @When("^user click on the  Credit card account$")
-    public void userClickOnTheCreditCardAccount() {
-        
+    public void userClickOnTheCreditCardAccount() throws Throwable {
+        CreditLimitIncreasePage.click_CreditCardAccount();
     }
-
     @And("^click the Request Increase button for Credit Card$")
-    public void clickTheRequestIncreaseButtonForCreditCard() {
-        
+    public void clickTheRequestIncreaseButtonForCreditCard() throws Throwable {
+        CreditLimitIncreasePage.click_RequestIncrease();
     }
-
     @Then("^I Verify the Application display the Pop Message$")
-    public void iVerifyTheApplicationDisplayThePopMessage() {
-        
+    public void iVerifyTheApplicationDisplayThePopMessage() throws Throwable {
+        CreditLimitIncreasePage.Verify_RIPopupMessage();
     }
-
+    @And("^I Click on GotIt button$")
+    public void iClickOnGotItButton() throws Throwable {
+        CreditLimitIncreasePage.click_GotIt();
+    }
     @And("^Check Application allow to enter \\(or\\) Slide less than the Current Limit$")
-    public void checkApplicationAllowToEnterOrSlideLessThanTheCurrentLimit() {
-
-
+    public void checkApplicationAllowToEnterOrSlideLessThanTheCurrentLimit() throws Throwable {
+        CreditLimitIncreasePage.click_Slider();
     }
 
     @And("^Check Application allow to enter the amount entered is above maximum allowed$")
@@ -34,4 +38,5 @@ public class CreditLimitIncrease {
     @And("^Check Application allow to enter the amount is not in increments of (\\d+)s$")
     public void checkApplicationAllowToEnterTheAmountIsNotInIncrementsOfS(int arg0) {
     }
+
 }
